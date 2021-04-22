1e91c85abb13f35f53091cecd46159b31b1ae49a
Taku Yoshioka <taku.yoshioka.4096@gmail.com>
2021-04-22 14:10:43 +0900

    Tweaks

--- examples/where4.rs
+++ examples/where4.rs
@@ -6,7 +6,7 @@ fn main() {
         .reshape(&[2, 5]);
     x.print();
     println!("=== Tensor::where4(x.lt(1), 0., 1.) ===");
-    let y = Tensor::where4(&x.lt(1), 0., 1.);
+    let y = Tensor::where4(&x.lt(3), 0., 1.);
     y.print();
 
     //     === x ===
@@ -14,7 +14,7 @@ fn main() {
     //     2  1  0  4  4
     //    [ CPUFloatType{2,5} ]
     //    === Tensor::where4(x.lt(1), 0., 1.) ===
-    //     1  1  1  1  1
-    //     1  1  0  1  1
+    //     1  0  1  1  1
+    //     0  0  0  1  1
     //    [ CPUFloatType{2,5} ]
 }
