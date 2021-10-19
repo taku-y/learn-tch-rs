b0fc0bc9e260077575fb96852e69d0a365a54925
Taku Yoshioka <taku.yoshioka.4096@gmail.com>
2021-10-19 14:00:03 +0900

    Add min example

--- examples/softmax.rs
+++ examples/softmax.rs
@@ -7,4 +7,17 @@ fn main() {
     let x = Tensor::hstack(&[x, pad]);
     println!("=== x.softmax(0, tch::Kind::Float) ===");
     x.softmax(0, tch::Kind::Float).print();
+    // === x.softmax(0, tch::Kind::Float) ===
+    //  0.0152
+    //  0.1125
+    //  0.8310
+    //  0.0414
+    //  0.0000
+    //  0.0000
+    //  0.0000
+
+    println!("=== x.softmax(0, tch::Kind::Float) ===");
+    let x = Tensor::of_slice(&[1f64, 3., 5., 2., 1e-128, 1e-256]).internal_cast_float(false);
+    x.softmax(0, tch::Kind::Float).print();
+
 }
