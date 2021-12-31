b3c66fa428cddcd081fa2fb0d5b4d7dd658fcf1e
Taku Yoshioka <taku.yoshioka.4096@gmail.com>
2021-12-31 21:34:54 +0900

    Update crossbeam example

--- examples/crossbeam.rs
+++ examples/crossbeam.rs
@@ -1,6 +1,6 @@
 use crossbeam_channel::unbounded;
 use std::{thread, convert::TryFrom};
-use tch::Tensor;
+use tch::{Tensor, nn::{self, Module}, Device};
 
 fn main() {
     let (s1, r) = unbounded();
@@ -15,4 +15,24 @@ fn main() {
     msg1.print();
     let msg2 = r.recv().unwrap();
     msg2.print();
+
+    let tensor1 = Tensor::try_from(vec![1., 2., 3.]).unwrap().internal_cast_float(false);
+    let vs1 = nn::VarStore::new(Device::Cpu);
+    let model1 = nn::seq()
+        .add(nn::linear(&vs1.root() / "layer1", 3, 8, Default::default()))
+        .add(nn::linear(&vs1.root() / "layer2", 8, 2, Default::default()));
+    let mut vs2 = nn::VarStore::new(Device::Cpu);
+    let model2 = nn::seq()
+        .add(nn::linear(&vs2.root() / "layer1", 3, 8, Default::default()))
+        .add(nn::linear(&vs2.root() / "layer2", 8, 2, Default::default()));
+
+    model1.forward(&tensor1).print();
+    model2.forward(&tensor1).print();
+
+    let (s, r) = unbounded();
+    thread::spawn(move || s.send(vs1).unwrap());
+    let vs3 = r.recv().unwrap();
+    vs2.copy(&vs3).unwrap();
+
+    model2.forward(&tensor1).print();
 }
